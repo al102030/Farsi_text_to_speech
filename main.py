4690ec9faaba0ba2e00f2e154bd682f37332383e
Ali Darvishi <ali.darvishi.k@gmail.com>
2024-06-17 14:20:47 +0330

    First simple and practical version

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ client = OpenAI(
 with open("text.txt", "r", encoding="utf-8") as my_file:
     txt = my_file.read()
 
-if not txt:
+if txt:
     completion = client.chat.completions.create(
         # Here you can choose your desire model
         model="gpt-3.5-turbo-0125",
@@ -28,19 +28,21 @@ if not txt:
     # this is the abstracted text which ChatGPT returned
     abstracted_txt = completion.choices[0].message.content
 
-# Ignore Deprecation Warning *
-warnings.filterwarnings("ignore", category=DeprecationWarning)
+    # Ignore Deprecation Warning *
+    warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-# here you can define a path to save your Audio file
-speech_file_path = Path(__file__).parent / "speech.mp3"
-response = client.audio.speech.create(
-    # This is the Text-to-speech API model
-    model="tts-1",
-    # this is the kind of Artificial voice
-    voice="shimmer",
-    input=abstracted_txt
-)
+    # here you can define a path to save your Audio file
+    speech_file_path = Path(__file__).parent / "speech.mp3"
+    response = client.audio.speech.create(
+        # This is the Text-to-speech API model
+        model="tts-1",
+        # this is the kind of Artificial voice
+        voice="shimmer",
+        input=abstracted_txt
+    )
 
-# * Despite the deprecation of this method, it remains the only one suggested by OpenAI.
-response.stream_to_file(speech_file_path)
-print("Done!")
+    # * Despite the deprecation of this method, it remains the only one suggested by OpenAI.
+    response.stream_to_file(speech_file_path)
+    print("Done!")
+else:
+    print("Text file is empty!")
